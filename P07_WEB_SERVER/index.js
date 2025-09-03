f3a84ef240ee40e00cb2603a8217d1af0fadaf39
Anshu <riyanshu062@gmail.com>
2025-09-03 18:32:55 +0530

    APIs and Routing

--- P07_WEB_SERVER/index.js
+++ P07_WEB_SERVER/index.js
@@ -1,16 +1,33 @@
-// Load the 'http' module to create a web server
+// Load the 'http' module to make a web server
 const http = require('http');
 
-// Choose the port where the server will run
+// Set the port number for the server
 const PORT = 3000;
 
-// Make a server that sends data when someone visits
+// Create the server
 const server = http.createServer((req, res) => {
-    // Tell browser we are sending JSON data with status 200 (OK)
-    res.writeHead(200, { 'Content-type': 'application/json' });
 
-    // Send a simple JSON object as response
-    res.end(JSON.stringify({ id: 1, name: 'Sir Isaac Newton' }));
+    // If the user visits /friends
+    if (req.url === '/friends') {
+        res.statusCode = 200; // Status OK
+        res.setHeader('Content-type', 'application/json'); // Tell browser it's JSON
+        res.end(JSON.stringify({ id: 1, name: 'Sir Isaac Newton' })); // Send JSON data
+
+    // If the user visits /messages
+    } else if (req.url === '/messages') {
+        res.setHeader('Content-type', 'text/html'); // Tell browser it's HTML
+        // Send simple HTML message
+        res.write('<html><body><ul>');
+        res.write('<li>Hello Isaac!</li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('</ul></body></html>');
+        res.end();
+
+    // For any other page
+    } else {
+        res.statusCode = 404; // Page not found
+        res.end();
+    }
 });
 
 // Start the server and show message in console
@@ -18,4 +35,4 @@ server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
 
-// 127.0.0.1 or localhost is the address to open this server in browser
+// 127.0.0.1 or localhost is used to open this server in browser
