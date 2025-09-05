9220fde3bf19b5290c4b81544d6b8c1b2eba7cee
Anshu <riyanshu062@gmail.com>
2025-09-05 17:18:03 +0530

    Revision of messages controller

--- P08_EXPRESS_PROJECT/controllers/messages.controller.js
+++ P08_EXPRESS_PROJECT/controllers/messages.controller.js
@@ -1,18 +1,29 @@
+// It helps in making safe file paths (works on all OS)
 const path = require('path');
 
+// Function to handle GET request (when user opens / messages page)
 function getMessages(req, res) {
+  // Show the 'messages' page and send two values (title and friend)
   res.render('messages', {
-    title: 'Messages to my Friends!',
-    friend: 'Elon Musk',
+    title: 'Messages to my Friends!',  // Page title
+    friend: 'Elon Musk',   // Friend name to show
   });
+
+  // (Optional) Instead of rendering the page, we can send an image file directly
   // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
 }
 
+// Function to handle POST request (when user submits a message)
 function postMessage(req, res) {
+  // Print a message in the server console
   console.log('Updating messages...');
+
+  // NOTE: No response is sent back to the user yet
+  // Should add res.send("Message saved!") or res.redirect("/messages")
 }
 
+// Export both functions so they can be used in other files (like routes.js)
 module.exports = {
-  getMessages,
-  postMessage,
+  getMessages,   // For GET request
+  postMessage,   // For POST request
 };
