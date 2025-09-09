95d9f7fa51a907cf79377c72efb10374f536f29a
Anshu <riyanshu062@gmail.com>
2025-09-09 20:58:52 +0530

    upto 25

--- P09_NASA_PROJECT/server/src/routes/launches/launches.controller.js
+++ P09_NASA_PROJECT/server/src/routes/launches/launches.controller.js
@@ -13,8 +13,18 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res){
     const launch = req.body;
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
     launch.launchDate = new Date(launch)
     addNewLaunch(launch);
+    if (isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
     return res.status(201).json(launch);
 }
 
