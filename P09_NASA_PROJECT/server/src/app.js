4ce1668770701eb1134226d7c617587edebd9b00
Anshu <riyanshu062@gmail.com>
2025-09-09 15:13:12 +0530

    upto 20

--- P09_NASA_PROJECT/server/src/app.js
+++ P09_NASA_PROJECT/server/src/app.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 const morgan = require('morgan'); 
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 // Create an Express application
 const app = express();
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(morgan('combined'));
 app.use(planetsRouter);
+app.use(launchesRouter);
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
---
+++ P09_NASA_PROJECT/server/src/routes/launches/launches.router.js
@@ -0,0 +1,16 @@
+// Import Express to create routes
+const express = require('express');
+
+// Importing the getAllLaunches Function from different module
+const {
+    getAllLaunches,
+} = require('./launches.controller');
+
+// Create a new router for launchesRouter
+const launchesRouter = express.Router();
+
+// Function to get all the launches information 
+launchesRouter.get('/launches', getAllLaunches);
+
+// Exporting the module so that we can use it 
+module.exports = launchesRouter;
