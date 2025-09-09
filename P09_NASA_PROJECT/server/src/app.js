b825d7fd62fd4dba8c6966f185f1c91f820076a4
Anshu <riyanshu062@gmail.com>
2025-09-09 11:40:03 +0530

    server

--- P09_NASA_PROJECT/server/src/app.js
+++ P09_NASA_PROJECT/server/src/app.js
@@ -3,6 +3,7 @@ This file sets up the main Express app. It adds important middlewares (like to r
  */
 
 // Load the Express library to create a web server and handle routes
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -16,7 +17,12 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 // Export the app so it can be used in other files (like server.js)
 module.exports = app;
