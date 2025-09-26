15f51dd26551a2cc7da23547893f7b271cd958fa
Anshu <riyanshu062@gmail.com>
2025-09-26 21:55:44 +0530

    Part 10 Completed

--- P09_NASA_PROJECT/server/src/routes/launches/launches.test.js
+++ P09_NASA_PROJECT/server/src/routes/launches/launches.test.js
@@ -23,7 +23,14 @@ describe('Test POST /launch', () => {
         mission: 'USS Enterprise',
         rocket: 'NCC 1701-D',
         target: 'Kepler-186 f',
-    }
+    };
+
+    const launchDataWithInvalidDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'zoot',
+    };
 
   test('It should respond with 201 created', async () => {
     const response = await request(app)
@@ -39,6 +46,15 @@ describe('Test POST /launch', () => {
     expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
-  test('It should catch missing required properties', () => {});
-  test('It should catch invalid dates', () => {});
+  test('It should catch missing required properties', async () => {
+    const response = await request(app)
+        .post('/launches')
+        .send(launchDataWithInvalidDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+    expect(response.body).toStrictEqual({
+        error: 'Invalid launch date',
+    });
+  });
 });
