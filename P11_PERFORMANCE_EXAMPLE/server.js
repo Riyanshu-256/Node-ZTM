8fbbf1f64635ecf3744b4723f0df91dea3611ba7
Anshu <riyanshu062@gmail.com>
2025-09-27 13:05:40 +0530

    Part 11 Completed

--- P11_PERFORMANCE_EXAMPLE/server.js
+++ P11_PERFORMANCE_EXAMPLE/server.js
@@ -1,22 +1,32 @@
+// We are using Express (a web framework for Node.js)
 const express = require('express');
 const app = express();
 
-function delay(duration){
+// This function just wastes time (4 sec) and blocks the server
+// While this runs, the server cannot handle other requests
+function delay(duration) {
     const startTime = Date.now();
-    while(Date.now() - startTime < duration){
-        //event loop is blocked
+    while (Date.now() - startTime < duration) {
+        // Waiting in a loop (bad for performance)
     }
 }
 
+// If someone visits http://localhost:3000/
+// This will reply quickly with the process ID
 app.get('/', (req, res) => {
     res.send(`Performance example: ${process.pid}`);
 });
 
+// If someone visits http://localhost:3000/timer
+// This will make the server "wait" 4 seconds before replying
 app.get('/timer', (req, res) => {
-    delay(4000);
+    delay(4000); // wait 4 seconds
     res.send(`Beep beep beep! ${process.pid}`);
 });
 
+// Just printing messages in terminal
 console.log('Running server.js...');
 console.log('Worker process started.');
-    app.listen(3000);
+
+// Start server at port 3000
+app.listen(3000);
