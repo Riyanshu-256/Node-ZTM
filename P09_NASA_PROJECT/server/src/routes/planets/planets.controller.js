0307dbbd3dc3fe7ec0be214ee2772a9de7614409
Anshu <riyanshu062@gmail.com>
2025-09-30 17:35:36 +0530

    Finding documents

--- P09_NASA_PROJECT/server/src/routes/planets/planets.controller.js
+++ P09_NASA_PROJECT/server/src/routes/planets/planets.controller.js
@@ -6,10 +6,10 @@ This file is like the “manager” for planets. It takes requests from users (l
 const { getAllPlanets } = require('../../models/planets.model');
 
 // Define a function to handle requests for "all planets"
-function httpGetAllPlanets(req, res) {
+async function httpGetAllPlanets(req, res) {
     // Send back a response with status 200 (OK) 
     // and return the planets data in JSON format
-    return res.status(200).json(getAllPlanets());
+    return res.status(200).json(await getAllPlanets());
 }
 
 // Export the function so it can be used in other files (like routes)
