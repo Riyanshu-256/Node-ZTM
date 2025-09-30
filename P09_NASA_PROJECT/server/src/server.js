856d9c87f6a6bd3fe1baa1bf3f8b82d819d97675
Anshu <riyanshu062@gmail.com>
2025-09-30 13:42:10 +0530

    Database part 01 completed

--- P09_NASA_PROJECT/server/src/server.js
+++ P09_NASA_PROJECT/server/src/server.js
@@ -4,6 +4,7 @@ This is the starting point of the project. It runs the app on a specific port so
 
 // Load Node.js module to create an HTTP server
 const http = require('http');
+const mongoose = require('mongoose');
 
 // Load your Express app which handles all routes and logic
 const app = require('./app');
@@ -14,19 +15,31 @@ const { loadPlanetsData } = require('./models/planets.model');
 // If no port is set, use 8000 as default
 const PORT = process.env.PORT || 8000;
 
+// Replace <db_password> with your actual password
+const MONGO_URL =
+  'mongodb+srv://nasa-api:mZW8omvljiskRwJf@nasacluster.kpehvk3.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster';
+
 // Create an HTTP server that uses your Express app to handle requests
 const server = http.createServer(app);
 
-async function startServer(){
-
-await loadPlanetsData();
-
-// Start the server and listen on the specified port
-// When the server starts, print a message in the console
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
 });
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+async function startServer() {
+  // Connect to MongoDB
+  await mongoose.connect(MONGO_URL);
+
+  await loadPlanetsData();
+
+  // Start the server and listen on the specified port
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+  });
 }
 
 startServer();
-//.....
