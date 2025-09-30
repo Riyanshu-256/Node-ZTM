875d12b119ffb51b73dcb7ddf798f112edc0fc8e
Anshu <riyanshu062@gmail.com>
2025-09-30 17:27:38 +0530

    Creating & Inserting document

--- P09_NASA_PROJECT/server/src/models/planets.model.js
+++ P09_NASA_PROJECT/server/src/models/planets.model.js
@@ -1,58 +1,39 @@
-/*
-This file is where all the planet data is managed. It decides how the data is stored, fetched, or updated. If later you connect a database, this file will handle that part, so the rest of the project doesn’t need to know how data is stored.
-*/
-
-// Import csv-parse to help read CSV files
 const { parse } = require('csv-parse');
 const path = require('path');
-
-// Import file system (fs) to read files
 const fs = require('fs');
+const planets = require('./planets.mongo');
 
-// Store all planets data here
 const results = [];
-
-// Store only life-supporting planets here
 const habitablePlanets = [];
 
-// Check if a planet can support life
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
 }
 
-// Load planets data from CSV file
 function loadPlanetsData() {
-
-    // Use Promise so we know when reading is done
     return new Promise((resolve, reject) => {
-
-        // Open the kepler_data.csv file
         fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-            // Send data to csv parser
             .pipe(parse({
-                comment: '#',   // Skip lines starting with #
-                columns: true,  // Use first row as column names
+                comment: '#',
+                columns: true,
             }))
-            // Runs when one row of data comes
-            .on('data', (data) => {
-                results.push(data);
-                
-                // Add to habitable list if planet is good for life
+            .on('data', async (data) => {
                 if (isHabitablePlanet(data)) {
-                    habitablePlanets.push(data);
+                    // TODO: Replace below create with insert + update = upset
+                    // await planets.create({
+                    //     KeplerName: data.kepler_name,
+                    // });
                 }
             })
-            // Runs if there is an error
             .on('error', (err) => {
                 console.log(err);
-                reject(err); // Stop if error happens
+                reject(err);
             }) 
-            // Runs after reading is complete
             .on('end', () => {
                 console.log(`Total habitable planets: ${habitablePlanets.length}`);
-                resolve(); // Finish promise
+                resolve();
             });
     });
 }
@@ -61,7 +42,6 @@ function getAllPlanets(){
     return habitablePlanets;
 }
 
-// Export function and data so other files can use them
 module.exports = {
     loadPlanetsData,
     getAllPlanets,
